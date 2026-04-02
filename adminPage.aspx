0c6d2750474d52b71b9b929331f18f140aa1e704
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-04-02 14:40:55 -0400

    Created Admin-Page branch, adding files for admin page (adminPage.xxx).

---
+++ adminPage.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+    CodeBehind="adminPage.aspx.cs"
+    Inherits="TutorBookings.adminPage" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Appointments</title>
+
+    <style>
+        body { font-family: Arial; }
+        table { border-collapse: collapse; width: 60%; }
+        th, td { border: 1px solid #ccc; padding: 8px; text-align: left; }
+        th { background-color: #eee; }
+    </style>
+</head>
+
+<body>
+    <form runat="server">
+        <h2>Appointments</h2>
+
+        <asp:GridView 
+            ID="AppointmentsGrid" 
+            runat="server" 
+            AutoGenerateColumns="false">
+
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="ID" />
+                <asp:BoundField DataField="StudentName" HeaderText="Student" />
+                <asp:BoundField DataField="TutorName" HeaderText="Tutor" />
+                <asp:BoundField DataField="CourseCode" HeaderText="Course Code" />
+                <asp:BoundField DataField="Date" HeaderText="Date" />
+                <asp:BoundField DataField="Time" HeaderText="Time" />
+                <asp:BoundField DataField="StudentEmail" HeaderText="Student Email" />
+
+                <asp:TemplateField HeaderText="Action">
+                    <ItemTemplate>
+                        <asp:Button 
+                            ID="DeleteButton"
+                            runat="server"
+                            Text="Delete"
+                            CommandArgument='<%# Eval("Id") %>'
+                            OnClick="DeleteButton_Click"
+                            OnClientClick="return confirm('Delete this appointment?');" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ adminPage.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TutorBookings
+{
+    public partial class adminPage : System.Web.UI.Page
+    {
+        
+        public class Appointment
+        {
+            public int Id { get; set; }
+            public string StudentName { get; set; }
+            public string TutorName { get; set; }
+            public string CourseCode { get; set; }
+            public string Date { get; set; }
+            public string Time { get; set; }
+            public string StudentEmail { get; set; }
+        }
+
+        
+        private static List<Appointment> appointments = new List<Appointment>
+        {
+            new Appointment { Id = 1, StudentName = "Moon", TutorName = "Webs", CourseCode = "PSY2100", Date = "3/20", Time = "2PM" },
+            new Appointment { Id = 2, StudentName = "Winter", TutorName = "Starflight", Date = "3/21", Time = "10AM" },
+            new Appointment { Id = 3, StudentName = "Peril", TutorName = "Clay", Date = "3/22", Time = "1PM", StudentEmail = "peril@jma.edu" }
+        };
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadAppointments();
+            }
+        }
+
+        
+        private void LoadAppointments()
+        {
+            AppointmentsGrid.DataSource = appointments;
+            AppointmentsGrid.DataBind();
+        }
+
+        
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            var button = (System.Web.UI.WebControls.Button)sender;
+            int id = int.Parse(button.CommandArgument);
+
+            
+            var appt = appointments.FirstOrDefault(a => a.Id == id);
+            if (appt != null)
+            {
+                appointments.Remove(appt);
+            }
+
+            
+            LoadAppointments();
+        }
+    }
+}
